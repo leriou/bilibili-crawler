d661ff449ba0f78bc09fc30446f9711f909b8c9d
leriou <yange1876@hotmail.com>
2019-01-06 21:39:33 +0800

    alter mongo save method

--- bilibili.py
+++ bilibili.py
@@ -1,99 +1,98 @@
-import di
 import tools
 from selenium import webdriver
+import time,sys
 
-class Builder:
+class Bilibili:
     
     def __init__(self):
         self.tools = tools.Tools()
-        self.di = di.Di()
-        self.redis = self.di.getRedis()
-        self.mongodb = self.di.getMongoDb()
-        self.browser = webdriver.Firefox()
-    
-    def url_map(self,flag,id):
+        self.mongodb = self.tools.get_mongodb()
+        self.dbname = "bilibili"
+        self.cache_name = "cache"
+        self.user_name = "users"
+        self.log_name = "logs"
+        self.tools.cache = self.mongodb[self.dbname][self.cache_name]
+        self.user_db = self.mongodb[self.dbname][self.user_name]
+       
+    # url map
+    def url_map(self, flag, id):
         if flag == 'user':
+            self._id = id
             return "https://space.bilibili.com/" + str(id)
 
-    def browser_get(self,url):
-        self.browser.get(url)
-        return self.browser.page_source
-
     def close_browser(self):
-        self.browser.close()
+        self.tools.close_browser()
 
-    def get_user_info(self):
-        url = self.url_map("user",123456)
-        cache_html =  self.redis.get(url)
-        if cache_html == None:
-            dom = self.tools.get_dom_by_html(self.browser_get(url))
-            self.redis.set(url,dom)
-        else:
-            dom = self.tools.get_dom_by_html(cache_html)
-
-        avatar = dom.select("#h-avatar")[0].get("src")
-        user_name = dom.select("#h-name")[0].string
-        gender = dom.select("#h-gender")[0].get("class")
-
-        if len(gender) == 3:
-            if gender[2] == 'female':
-                gender = "女"
-            elif gender[2] == 'male':
-                gender = "男"
-        else:
-            gender = "未知"
-        
-        level = dom.select(".h-level")[0].get("lvl")
-        vip = dom.select(".h-vipType")[0].get("class")
-
-        if len(vip) == 2:
-            if vip[1] == 'normal-v':
-                vip = "大会员"
-            else:
-                vip = "年费大会员"
-        elif len(vip) == 1:
-            vip = "否"
-        sign = dom.select(".h-sign")[0].string.strip()
-        # print(user_name)
-        uid = dom.select(".section .uid span")[1].string
-        reg_time =  dom.select(".section .regtime span")[1].string.strip()
-        birthday = dom.select(".section .birthday span")[1].string.strip()
-        address =  dom.select(".section .geo span")[1].string.strip()
-
-        videos = dom.find(href="#/video").select("span")[2].string # 投稿
-    
-        n_statistics = dom.select(".n-statistics")[0]
-
-        focus =  n_statistics.select("#n-gz")[0].string  # 关注数
-        fans =   n_statistics.select("#n-fs")[0].string  # 粉丝数
-        if len(n_statistics.select("#n-bf")) == 1:
-            plays = n_statistics.select("#n-bf")[0].string   # 播放数
-        else:
-            plays = 0
-            
+    # 获取某个用户的信息
+    def get_user_info(self, url, save_user = True):
+        if self.tools.check_url_success(url):
+            return False
+        dom = self.tools.get_dom_obj(url, False)
+        # 解析dom页面
         user_info = {
-            "avatar":avatar,
-            "username":user_name,
-            "gender":gender,
-            "level":level,
-            "vip":vip,
-            "sign":sign,
-            "uid":uid,
-            "regtime":reg_time,
-            "birthday":birthday,
-            "address":address,
-            "videos":videos,
-            "focus":focus,
-            "fans":fans,
-            "plays":plays
+            "_id": self._id
         }
-        self.close_browser()
-        print(user_info)
-        self.save_user_info(user_info)
-        return 
+        if dom.select("#h-avatar"):
+            user_info["avatar"] = dom.select("#h-avatar")[0].get("src")
+        if dom.select("#h-name"):
+            user_info["username"] = dom.select("#h-name")[0].string
+        if dom.select(".h-level"):
+            user_info["level"] = dom.select(".h-level")[0].get("lvl")
+        if dom.select("#h-gender"):
+            gender = dom.select("#h-gender")[0].get("class")
+            if len(gender) == 3:
+                user_info["gender"] = gender[2]
+            else:
+                user_info["gender"] = ""
+        if dom.select(".h-vipType"):
+            vip = dom.select(".h-vipType")[0].get("class")
+            if len(vip) == 2:
+                if vip[1] == 'normal-v':
+                    vip = "大会员"
+                else:
+                    vip = "年费大会员"
+            elif len(vip) == 1:
+                vip = "否"
+            user_info["vip"] = vip
+        if dom.select(".h-sign"):
+            user_info["sign"] = dom.select(".h-sign")[0].string.strip()
+        if dom.select(".user .uid .text"):
+            user_info["_id"] = dom.select(".user .uid .text")[0].string.strip()
+        if dom.select(".user .regtime .text"):
+            user_info["regtime"] =  dom.select(".user .regtime .text")[0].string.strip().strip("注册于 ")
+        if dom.select(".user .birthday .text"):
+            user_info["birthday"] = dom.select(".user .birthday .text")[0].string.strip()
+        if dom.select(".n-video .n-num"):
+            user_info["videos"] = dom.select(".n-video .n-num")[0].string 
+        if dom.select(".n-favlist .n-num"):
+            user_info["favlist"] = dom.select(".n-favlist .n-num")[0].string.strip()
+        if dom.select(".n-statistics .n-gz"):
+            user_info["focus"] = dom.select(".n-statistics .n-gz")[0].get("title").replace(",","")  
+        if dom.select(".n-statistics .n-fs"):
+            user_info["fans"] = dom.select(".n-statistics .n-fs")[0].get("title").replace(",","")
+        plays = 0
+        if dom.select(".n-statistics .n-bf"):
+            plays = dom.select(".n-statistics .n-bf")[0].get("title").replace(",","")
+        user_info["plays"] = plays
+        self.tools.logging("INFO",user_info)
+        if save_user:
+            self.save_user_info(user_info)
+        return user_info
         
     def save_user_info(self,user_info):
-        self.mongodb["users"]['bilibili'].insert(user_info)
+        self.user_db.replace_one({"_id": user_info["_id"]} , user_info, True)
     
     def run(self):
-        self.get_user_info()  
+        uid = 1
+        loop = True
+        while loop:
+            if len(sys.argv) == 3:
+                uid = int(sys.argv[2])
+                loop = False
+            url = self.url_map("user", uid)
+            time.sleep(2)
+            uid = uid + 1
+            user_info = self.get_user_info(url)
+            if user_info:
+                self.tools.marked_url_success(url)
+        self.close_browser()
--- tools.py
+++ tools.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import urllib.request
-import requests
 import os
+import time
 from bs4 import BeautifulSoup
-
+from selenium import webdriver
+from pymongo import MongoClient
+import random
 
 
 class Tools:
-
+    
+    def __init__(self):
+        self.browser = None
+        self.sleep = False
+        self.rand_floor = 0
+        self.rand_ceil = 3
+        self.start = time.time()
+        self.end = time.time()
+        self.mongo = None
+    
+    def get_mongodb(self):
+        if self.mongo == None:
+            self.mongo = MongoClient('127.0.0.1', 27017)
+        return self.mongo
+    
+    def set_cache(self, db, collection):
+        self.cache = self.mongo[db][collection]
+    
     # 从url获取页面内容
     def get_html(self, url):
         try:
             times = 3    
             while times > 0:
                 param_data = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"}
-                response = requests.get(url,params=param_data)
-                times = times - 1
+                response = requests.get(url, params=param_data)
+                times -= 1
                 if response.status_code == 200:
                     times = False
             if response.status_code == 200:
@@ -26,14 +45,67 @@ class Tools:
                 return False
         except:
             return False
-        
-    def get_dom_by_html(self,html):
-        return  BeautifulSoup(html,'html.parser')
 
-    # 获取页面的dom对象
-    def get_dom_obj(self, url):
-        data = self.get_html(url)
+    # 关闭浏览器
+    def close_browser(self):
+        if self.browser != None:
+            self.browser.close()
+
+    # 浏览器获取
+    def browser_get_html(self, url):
+        if self.browser == None:
+            self.browser = webdriver.Chrome()     
+        if self.sleep:
+            time.sleep(self.get_random_num(self.rand_floor, self.rand_ceil))
+        self.browser.get(url)
+        return self.browser.page_source
+
+    def mongo_set(self, url, data):
+        r = self.cache.find_one({"_url": url})
+        if r:
+            self.cache.update({"_url":url}, {"$set":data})
+        else:
+            data["_url"] = url
+            data["datetime"] = self.get_time()
+            data["timestamp"] = time.time()
+            self.cache.insert_one(data)
+        
+    def mongo_get(self,url):
+        return self.cache.find_one({"_url":url})
+
+    def mongo_clear_cache(self):
+        self.cache.update_many({},{"$set":{"text":""}})
+
+    # 从html字符串获取dom对象
+    def get_dom_by_html(self,html):
+        if html == None:
+            return False
+        return BeautifulSoup(html, 'html.parser')
+
+    # 标记某个url处理成功
+    def marked_url_success(self,url,flag = 1):
+        self.mongo_set(url,{"success":flag})
+
+    # 检查某个url是否成功
+    def check_url_success(self,url):
+        r = self.mongo_get(url)
+        if r and r.get("success"):
+            return r["success"]
+        return False
+
+    # 获取页面的dom对象如果页面被缓存使用缓存
+    def get_dom_obj(self, url, cached=True, browser=True):
+        if cached:
+            r = self.mongo_get(url)
+            if r != None and r.get("text") :
+                return self.get_dom_by_html(r["text"])
+        if browser:
+            data = self.browser_get_html(url)
+        else:
+            data = self.get_html(url)
         if data:
+            if cached:
+                self.mongo_set(url,{"text":data}) 
             return self.get_dom_by_html(data)
         else:
             return False
@@ -49,8 +121,27 @@ class Tools:
             return newpath
 
     # 往某文件写入内容
-    def log(self,filename,content):
-        fh = open(filename,"w+")
-        fh.write(content)
-        fh.close()
+    def log_to_file(self,filename,content):
+        with open(filename,"w+") as fh:
+            fh.write(content)
 
+    def get_time(self):
+        return self.time2str(time.time())
+    
+    def str2time(self,i):
+        return time.mktime(time.strptime(i,"%Y-%m-%d %H:%M:%S"))
+    
+    def time2str(self,moment):
+        return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(moment))
+        
+    def cost(self, log=''):
+        tmp = time.time()
+        total, last = tmp - self.start, tmp - self.end
+        self.end = tmp
+        self.logging("INFO"," %s cost time: %s s, until lastest %s s" % (log, total, last))
+
+    def logging(self,level,msg):
+        print("%s [%s]: %s" % (self.get_time(),level,msg))
+
+    def get_random_num(self, start=0, end=1):
+        return random.random() * (end - start) + start
